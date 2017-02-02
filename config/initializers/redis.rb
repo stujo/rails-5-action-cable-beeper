0abf26b1bde3d42aafd803a840c3c36b2225a954
Stujo <stujo@users.noreply.github.com>
2017-02-02 15:30:52 -0800

    Debugging REDIS

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,10 @@
+puts "Setting up Redis Initializer"
+
 Redis.class_eval do
   def self.connect_to_redis!
     uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379")
+    puts "REDIS"
+    p uri
     $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
   end
 end
